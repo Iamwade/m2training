fb1c488eaf4d9c2be745fa19e7c8c3e7d0e5e870
serhii <sergey.ak@interactivated.me>
2019-06-15 18:13:30 +0300

    3.14 добавлена форма на кастомную страницу через viewModel

---
+++ app/code/Training/Render/ViewModel/Form.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Training\Render\ViewModel;
+
+
+class Form implements \Magento\Framework\View\Element\Block\ArgumentInterface
+
+{
+    /**
+     * @var \Magento\Framework\UrlInterface
+     */
+    private $urlBuilder;
+
+    /**
+     * Form constructor.
+     * @param \Magento\Framework\UrlInterface $urlBuilder
+     */
+    public function __construct(
+        \Magento\Framework\UrlInterface $urlBuilder
+    ) {
+        $this->urlBuilder = $urlBuilder;
+
+    }
+
+    public function getSubmitUrl()
+    {
+        return $this->urlBuilder->getUrl('customer/account/login');
+    }
+}
