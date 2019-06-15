0577d69df75fce7c6c2890dba5cc320e6175c8b0
serhii <sergey.ak@interactivated.me>
2019-06-15 12:39:55 +0300

    3.3 контроллер возвращает обьект результата типа Row

--- app/code/Training/Test/Controller/Block/Index.php
+++ app/code/Training/Test/Controller/Block/Index.php
@@ -10,15 +10,23 @@ class Index extends \Magento\Framework\App\Action\Action
     private $layoutFactory;
 
     /**
+     * @var \Magento\Framework\Controller\Result\RawFactory
+     */
+    private $resultRawFactory;
+
+    /**
      * Index constructor.
      * @param \Magento\Framework\App\Action\Context $context
      * @param \Magento\Framework\View\LayoutFactory $layoutFactory
+     * @param \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
      */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\LayoutFactory $layoutFactory
+        \Magento\Framework\View\LayoutFactory $layoutFactory,
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
     ) {
         $this->layoutFactory = $layoutFactory;
+        $this->resultRawFactory = $resultRawFactory;
         parent::__construct($context);
     }
 
@@ -26,8 +34,8 @@ class Index extends \Magento\Framework\App\Action\Action
     {
         $layout = $this->layoutFactory->create();
         $block = $layout->createBlock('Training\Test\Block\Test');
-        return $this->getResponse()->appendBody($block->toHtml());
-
+        $html = $block->toHtml();
+        return $this->resultRawFactory->create()->setContents($html);
     }
 }
 
