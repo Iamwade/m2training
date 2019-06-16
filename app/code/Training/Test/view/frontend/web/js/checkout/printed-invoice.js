5812e33fdd5f48abd17c5de59fbafc8f4d798e45
serhii <sergey.ak@interactivated.me>
2019-06-16 14:42:45 +0300

    4.3: создан js файл который через mixin добавлен на страницу чекаута. также с помощью лайаута в чекаут был добавлен чекаут (html и js файлы)

---
+++ app/code/Training/Test/view/frontend/web/js/checkout/printed-invoice.js
@@ -0,0 +1,12 @@
+define([
+    'uiComponent',
+    'jquery',
+    'ko'
+], function(Component, $, ko) {
+    'use strict';
+
+    return Component.extend({
+        //for possible additional functionality
+
+    })
+});
